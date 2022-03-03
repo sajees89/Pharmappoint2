60bfae88661b27be907bf09b9f455e0c5e1fd91e
Sajees XPS <sajees.jeya@gmail.com>
2022-03-03 17:44:49 -0500

    update appointment

--- controllers/view.js
+++ controllers/view.js
@@ -41,7 +41,60 @@ router.get('/appointment', (req, res) => {
   });
 
 
-  
+// Get an appointment
+router.get('/:id', (req, res) => {
+  Appointment.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['Appointments_time', 'Appointments_date', 'Appointments_type'],
+      include: [
+        {
+           model: User,
+           attributes: ['email'],
+        },
+        {
+          model: Time,
+          attributes: ['time']
+        },
+     ],
+    })
+    .then(dbAppointmentData => {
+      const appointment = dbAppointmentData.map(post => post.get({ plain: true })); 
+      console.log(appointment)
+      res.render('appointment', {
+        appointment,
+        logggedIn: req.session.loggedIn || false
+      });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+//Create an Appointment
+router.post('/', (req, res) => {
+  Appointment.create({
+    Appointments_time: req.body.Appointments_time,
+    Appointments_date: req.body.Appointments_date,
+    Appointments_type: req.body.Appointments_type,
+  })
+  .then(dbAppointmentData => {
+    const appointment = dbAppointmentData.map(post => post.get({ plain: true })); 
+    console.log(appointment)
+    res.render('appointment', {
+      appointment,
+      logggedIn: req.session.loggedIn || false
+    });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
+
 
 
 module.exports = router;
